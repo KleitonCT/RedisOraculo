25ea554d4155cb061ba8826c0a77681d6bd1fd8d
Kleiton da Cunha Tangioni <kleitonct@hotmail.com>
2019-12-19 22:19:28 -0200

    Add project files.

---
+++ RedisOraculo/Program.cs
@@ -0,0 +1,26 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace RedisOraculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Connect
+            var redis = ConnectionMultiplexer.Connect("13.65.194.91");
+            IDatabase db = redis.GetDatabase();
+            string channel = "Perguntas";
+
+
+            var sub = redis.GetSubscriber();
+            sub.Subscribe(channel, (ch, msg) =>
+            {
+                string pergunta = msg.ToString().Split(":")[0];
+                sub.Publish(ch, string.Concat(pergunta, "Resposta"));
+            });
+        }
+    }
+}
